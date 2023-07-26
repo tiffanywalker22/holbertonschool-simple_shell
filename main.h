a821b91d5b6bb4994928a715c21211778929d493
tiffanywalker22 <tbcasper16@gmail.com>
2023-07-26 09:51:43 -0700

    adding main file

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/* header includes */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+
+/* function prototypes */
+
+
+
+#endif
