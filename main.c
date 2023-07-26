18c3f939d9d5ebc873bc96100e79df1facdfe31e
Zal-atan <ethanzalta@yahoo.com>
2023-07-26 10:19:41 -0700

    Added macros to main.h, created main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - 
+ * 
+ * @argc - count of arguments given
+ * 
+ * @argv - array of arguments given
+ * 
+ * Return: 0 on success?
+*/
--- main.h
+++ main.h
@@ -1,5 +1,17 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE -1
+
+#define ERROR_MALLOC -77
+#define EXIT_F_STAT -23
+#define EXIT_F_ACCESS -24
+#define PROMPT "How may I serve you? ;)"
+#define WHITESPACE "\n\t\r"
+#define PATH_DELIM ":"
+#define BUFF_SIZE 1024
+
 /* header includes */
 #include <stdio.h>
 #include <string.h>
